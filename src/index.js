beac453d9edbf64744888212c66aafb85944483b
Samuel Degife <samuel@Samuels-MacBook-Pro.local>
2022-01-08 16:50:09 -0600

    fetch git api using react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function GithubUser({login}){
+  const [data, setData] = useState(null);
+  useEffect(()=>{
+    fetch(`https://api.github.com/users/${login}`)
+    .then(res => res.json())
+    .then(setData)
+    .catch(console.error);
+  }, [login]);
+
+  if(data){
+    return (
+      <div>
+        <h1>{data.login}</h1>
+        <img src={data.avatar_url} width={100} alt={""}/>
+      </div>
+    )
+  }
+
+  return null;
+}
+
+function App() {
+  
+    return <GithubUser login="samimideksa"/>
+  
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
